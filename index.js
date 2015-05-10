6772e138063ff4e1c774d51f1f89b579889a2a7b
SteffenGorenflo <SteffenGorenflo@gmail.com>
2015-05-10 13:42:41 +0200

    add regex

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+    /**
+     * To test against the content type provided by hapi.
+     */
+    imageContentType: /^image\/(?:jpg|png|jpeg)$/,
+
+    /**
+     * Check against a file extension
+     */
+    imageExtension: /^jpg|png|jpeg$/
+
+};
