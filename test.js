97420ee0d7de1d84a362ac60a67feb7aff2f868b
SteffenGorenflo <SteffenGorenflo@gmail.com>
2015-06-23 22:48:33 +0200

    better validation and tests

--- test.js
+++ test.js
@@ -1,7 +1,10 @@
+'use strict';
 var util = require('./');
 var assert = require('assert');
+var Joi = require('joi');
 
 var regex = util.regex;
+var validation = util.validation;
 
 // positive tests
 assert(regex.imageContentType.test("image/png"));
@@ -16,4 +19,22 @@ assert(regex.imageExtension.test("jpeg"));
 // negative tests
 assert(!regex.imageContentType.test("image/gif"));
 assert(!regex.imageExtension.test("gif"));
+
+// validate joi validation
+var testObject = {
+    width: 500,
+    height: 500,
+    xCoord: 0,
+    yCoord: 0,
+    file: {
+        hapi: {
+            headers: {
+                'content-type': 'image/jpg'
+            }
+        }
+    }
+};
+Joi.validate(testObject, validation.basicImageSchema, function (err, value) {
+    assert(!err);
+});
 
